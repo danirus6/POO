ef306da4819b043dc2cf3f6d8d1dd47a3b954fa9
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-30 20:06:43 +0100

    Ejercicios acabados (faltan extras)

--- main.js
+++ main.js
@@ -6,9 +6,38 @@ class Persona {
 
     }
     obtDetalles = function () {
-        return `Nombre: ${this.nombre}\n Edad: ${this.edad}\n genero: ${this.genero}`
+        return `Nombre: ${this.nombre}\nEdad: ${this.edad}\nGenero: ${this.genero}`
     }
 }
-const persona = Persona("Dani", 25, "Masc")
+const persona = new Persona("Dani", 25, "Masc")
 persona.obtDetalles()
 console.log(persona.obtDetalles());
+
+//2
+
+class Estudiante extends Persona {
+    constructor(nombre, edad, genero, curso, grupo) {
+        super(nombre, edad, genero);
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+    registrar() {
+        return `Nombre: ${this.nombre}\nEdad: ${this.edad}\nGenero: ${this.genero}\nCurso: ${this.curso}\nGrupo: ${this.grupo}`
+    }
+}
+const estudiante = new Estudiante("Juan", 18, "Masc", "2º", "D");
+console.log(estudiante.registrar());
+//3
+class Profesor extends Persona {
+    constructor(nombre, edad, genero, asignatura, nivel) {
+        super(nombre, edad, genero);
+        this.asignatura = asignatura;
+        this.nivel = nivel;
+    }
+    asignar() {
+        return `Nombre: ${this.nombre}\nEdad: ${this.edad}\nGenero: ${this.genero}\nAsignatura: ${this.asignatura}\nNivel: ${this.nivel}`
+    }
+}
+
+const profe = new Profesor("Lara", 20, "Fem", "Programación", "3");
+console.log(profe.asignar());
